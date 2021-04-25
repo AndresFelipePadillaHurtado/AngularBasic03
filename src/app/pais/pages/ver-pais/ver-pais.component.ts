cdab32f44dd52c6cd7803bd59c45297fc49f0329
Andres Felipe Padilla Hurtado <afpadilla84@misena.edu.co>
2021-04-25 13:07:39 -0500

    Mostrar el template de la informacion de un pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interfaces';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,6 +11,8 @@ import { PaisService } from '../../services/pais.service';
   ]
 })
 export class VerPaisComponent implements OnInit {
+
+  pais!: Country; // ! indicarle que pais puede ser nulo
 
   //constructor Se ejecuta antes del inicializar el componenete
   constructor(
@@ -23,11 +26,10 @@ export class VerPaisComponent implements OnInit {
     // Uso del operador switcMap, este recibe un Observable y retorna otro Observable
     this.activatedRoute.params
     .pipe(
-      switchMap( (params) => this.paisService.getPaisPorAlpha(params.id) )
+      switchMap( (params) => this.paisService.getPaisPorAlpha(params.id) ),
+      tap( console.log ) // El operador tap sirve para poner en resultado de la consola
     )
-    .subscribe( resp => {
-
-    });
+    .subscribe( pais => this.pais = pais );
 
     // this.activatedRoute.params
     // .subscribe( params =>{ // Los parametro de las rutas
